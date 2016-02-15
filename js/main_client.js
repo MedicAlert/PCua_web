6b2950f2e79b21182a3615fea5f89d2bc2a542be
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2016-02-14 19:09:52 -0600

    Completed Download

--- js/main_client.js
+++ js/main_client.js
@@ -34,7 +34,23 @@ var stateGrub =
 	var ranCho2 = Math.floor(Math.random() * stateGrub.length);
 	var ranInt = Math.floor((Math.random()* 9999) + 1000);
 
-	var conCat = stringGrub[ranCho1] + "-" + ranInt + " - [" + stateGrub[ranCho2] + "]";
+	var conCat = stringGrub[ranCho1] + "-" + ranInt + " - [" + stateGrub[ranCho2] + "... ]";
 
 	$('#window_id').text(conCat);
+
+	$('.windows').hover(function() {
+		$('#qbox').show();
+		$('#qbox').empty();
+		$('#qbox').append("<h4>Requirements</h4><ul><li>Windows Vista or above</li><li>At least 1 core CPU</li><li>At least 2 GB of RAM</li><li>At least 100 GB of diskspace</li><li>An internet connection (DSL or better)</li></ul>	");
+	});
+
+	$('.github').hover(function() {
+		$('#qbox').show();
+		$('#qbox').empty();
+		$('#qbox').append("<h4>See how it's done!</h4><p>We're a bunch of lazy bastards with computers, so we let the power of programming take care of fetching and compiling the best hardware for you. If you want to know how we did things and want to contribute check out our GitHub repo.</p>	");	
+	});
+
+	$('.doc').hover(function() {
+		$('#qbox').hide();
+	});
 });
